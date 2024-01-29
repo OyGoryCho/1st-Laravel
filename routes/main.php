335318af02d671fba4bf463fd709bb5d329dbc52
OyGoryCho <medvedev.ihenco0706@gmail.com>
2024-01-30 02:07:21 +0300

    second commit

---
+++ routes/main.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+Route::redirect('/home', '/');
+
+Route::get('/test', TestController::class);     //$ php artisan make:controller TestController
+
+Route::get('blog', [BlogController::class, 'index'])->name('blog');
+Route::get('blog/{post}', [BlogController::class, 'show'])->name('blog.show');
+
+
+
+//CRUD (create, read, update, delete)
+
+//route::fallback() если не нашлось ничего 
